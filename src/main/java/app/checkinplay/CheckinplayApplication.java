730e697670bd70a4f0866400241110890eff64a9
Hugo Simões <hugosimoescontato@gmail.com>
2026-03-06 17:47:24 -0300

    initial setup

---
+++ src/main/java/app/checkinplay/CheckinplayApplication.java
@@ -0,0 +1,13 @@
+package app.checkinplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/checkinplay/CheckinplayApplicationTests.java
@@ -0,0 +1,13 @@
+package app.checkinplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
